eeafde5ab9311ff7dcc56fb4e3886291d71861e7
PRASUN SINGH <30758143+PRASUN95@users.noreply.github.com>
2020-11-08 00:36:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LinqProgramCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //count file extensions
+
+            #region
+            //string[] files = new string[]
+            //{
+            //    "str.txt","asd.png","ast.doc","wst.TXT","xyz.abc.pdf","aaaa.PDF","abc.xml"
+            //};
+            //var excnt = files.Select(file => Path.GetExtension(file).TrimStart('.').ToLower()).
+            //    GroupBy(x => x, (ext, extCount) => new
+            //    {
+            //        Extension = ext,
+            //        ExtCount = extCount.Count()
+            //    });
+            //foreach(var ob in excnt)
+            //{
+            //    Console.WriteLine("{0} {1}",ob.Extension,ob.ExtCount);
+            //}
+
+            //int[] a = new int[]
+            //{
+            //    3,4,2,4,5,6
+            //};
+            ////   Array.Sort(a, new ValueComparator());
+            //Array.Sort(a, (x, y) => x.CompareTo(y));
+            //string s = "";
+            //foreach(int el in a)
+            //{
+            //    s += el.ToString();
+            //}
+            //Console.WriteLine(s);
+            #endregion
+            int[] a = new int[]
+            {
+                3,4,2,4,5,6
+            };
+            LinqPrograSmCsharp.Sorting.BubbleSort(ref a);
+            printArray(a);
+            Console.ReadKey();
+        }
+
+        static void printArray(int[] a)
+        {
+            foreach (var e in a)
+                Console.Write("{0} ", e);
+        }
+    
+    }
+  
+    public class ValueComparator : IComparer
+    {
+        public int Compare(object ob1, object ob2)
+        {
+            string a = ob1.ToString(), b = ob2.ToString();
+            string x = a + b, y = b + a;
+            //Console.WriteLine($"{x} {y} {string.Compare(x,y)}");
+            return y.CompareTo(x);
+        }
+    }
+}
+
+
+public class Solution
+{
+    public string LargestNumber(int[] nums)
+    {
+        Array.Sort(nums, (x, y) => (y + "" + x).CompareTo(x + "" + y));
+        return nums[0] == 0 ? "0" : string.Join("", nums);
+    }
+}
+
+public class Solution1
+{
+    public string LargestNumber(int[] nums)
+    {
+        var strNums = nums.Select(n => n.ToString()).ToArray();
+        Array.Sort(strNums, (a, b) => string.Compare(b + a, a + b, StringComparison.Ordinal));
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var w in strNums)
+            sb.Append(w);
+        string ans = sb.ToString().TrimStart('0');
+        return ans == "" ? "0" : ans;
+
+    }
+}
---
+++ Sorting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LinqPrograSmCsharp
+{
+    static class Sorting
+    {
+        public static void BubbleSort(ref int[] a)
+        {
+            int temp = 0;
+            bool flag = true;
+            for (int i = 0; i < a.Length; i++)
+            {
+                flag = true;
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] < a[j])
+                    {
+                        flag = false;
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+                if (flag) break;
+            }
+        }
+    }
+}
